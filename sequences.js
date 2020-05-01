0deb7b77c7e4bbfb6b0e02ffebe61360ec4fdd3c
Bence Fábián <begnoc@gmail.com>
2020-05-01 16:01:26 +0200

    then

--- sequences.js
+++ sequences.js
@@ -82,3 +82,33 @@ function* concatenate(sequence) {
 }
 
 // Array.from(take(4, concatenate(repeat([true, false]))))
+
+// [1, 2, 3, 4] * ['a', 'b', 'c'] -> [[1, 'a'], [1, 'b'], ...]
+
+// seq1.then(v1 =>
+//           seq2.then(v2 =>
+//                     yield [v1, v2]))
+
+// then(seq, v1 =>
+//      then(seq2, v2 =>
+//           singletonSeq[v1, v2]))
+
+// singletonSeq
+//
+// A sequence yielding once
+function* singletonSeq(value) {
+  yield value;
+}
+
+// then
+//
+// Do something with each element returning new sequences
+// flattening the result.
+function then(sequence, transform) {
+  return concatenate(map(transform, sequence));
+}
+
+// Array.from(then([1, 2, 3], each => singletonSeq(each)))
+// Array.from(then([1, 2, 3], v1 =>
+//                 then(['a', 'b', 'c'], v2 =>
+//                      singletonSeq([v1, v2]))))
